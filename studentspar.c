a0e0a2de6c519a4885ad71249da10a800cffcf7c
Leonardo Chagas Pizzo <leonardo_pizzo@usp.br>
2022-10-27 14:05:00 -0300

    tseq = tpar*2.5

--- studentspar.c
+++ studentspar.c
@@ -51,20 +51,25 @@ int main()
     int n_threads = NUM_THREADS;
     omp_set_num_threads(n_threads);
     // #pragma omp parallel for
-    clock_t t1 = clock();
+    // clock_t t1 = clock();
 
+    #pragma omp parallel for
     for (int i = 0; i < R; i++)
     {
-        // #pragma for
+        // #pragma omp for
         for (int j = 0; j < C; j++)
         {
-            #pragma omp parallel 
+            #pragma omp parallel
             {
                 int *bucketTemp = (int *)calloc(LIM_NOTAS + 1, sizeof(int));
+                // clock_t tTemp1 = clock();
                 #pragma omp simd 
                 for (int k = omp_get_thread_num(); k < A; k+= NUM_THREADS){
                     bucketTemp[M[i][j][k]]++;
                 }
+                // clock_t tTemp2 = clock();
+                // printf("thread:%d\ntempo contagem cidade: %f\n", omp_get_thread_num(), (float)(tTemp2-tTemp1) / CLOCKS_PER_SEC);
+
                 // #pragma omp for reduction(+: bucketsCidades[i*C+j][:LIM_NOTAS + 1])
                 for (int k = 0; k < LIM_NOTAS + 1; k++)
                 {
@@ -80,16 +85,14 @@ int main()
             resultadosCidades[j + (i * C)][4] = obterDesvioPadrao(A, resultadosCidades[j + (i * C)][3], bucketsCidades[j + (i * C)]);
         }
     }
-    clock_t t2 = clock();
-    printf("tempo: %f\n", (float)(t2-t1) / CLOCKS_PER_SEC);
-    //#pragma omp parallel for
+    // clock_t t2 = clock();
+
+    // printf("tempo contagem das notas: %f\n", (float)(t2-t1) / CLOCKS_PER_SEC);
     for (int i = 0; i < R; i++)
     {
         // reduzir os buckets cidade pros buckets regiao
-        //#pragma for
         for (int j = 0; j < C; j++)
         {
-            //#pragma omp for reduction (+:bucketsRegioes[i][:LIM_NOTAS + 1])
             for (int k = 0; k < LIM_NOTAS + 1; k++)
             {
                 bucketsRegioes[i][k] += bucketsCidades[(i * C) + j][k];
@@ -104,28 +107,21 @@ int main()
         resultadosRegioes[i][4] = obterDesvioPadrao(A * C, resultadosRegioes[i][3], bucketsRegioes[i]);
     }
     
-    //#pragma omp parallel
-    //{
-        // #pragma omp for reduction (+: bucketGeral[:LIM_NOTAS + 1])
-        for (int i = 0; i < R; i++)
-        {
+    for (int i = 0; i < R; i++)
+    {
         // reduzir os buckets regiao pro bucket geral
-            for (int j = 0; j < LIM_NOTAS + 1; j++)
-            {
-                bucketGeral[j] += bucketsRegioes[i][j];
-            }
+        for (int j = 0; j < LIM_NOTAS + 1; j++)
+        {
+            bucketGeral[j] += bucketsRegioes[i][j];
         }
-    // }
+    }
 
     // calcular os resultados da regiao      
-    //#pragma omp parallel
-    //{ 
     resultadosGerais[0] = obterMin(bucketGeral);
     resultadosGerais[1] = obterMax(bucketGeral);
     resultadosGerais[2] = obterMediana(R*C*A, bucketGeral);
     resultadosGerais[3] = obterMedia(R*C*A, bucketGeral);
     resultadosGerais[4] = obterDesvioPadrao(A * R * C, resultadosGerais[3], bucketGeral);
-    //}
     
     clock_t end = clock();
 
